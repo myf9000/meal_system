d9f811a5cc4a6a6599ae0c4d090dd8b113f1ee4e
myf9000 <myf9000@vp.pl>
2016-03-17 11:41:05 +0100

    Small refactor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,21 @@
 class OrdersController < ApplicationController
 	before_action :meal, only: :create
 	before_action :order, only: [:destroy, :delivered]
-	
+
 	def index
 		respond_with current_user.orders
 	end
 
 	def create
-    order = meal.create_order(order_params.merge(user_id: current_user.id))
+    order = meal.create_order(order_params.merge(user: current_user))
     respond_with order
   end
 
   def destroy
-  	order.destroy 
+  	order.destroy
   	render json: {status: :ok}
   end
-  
+
   def delivered
   	order.update_attributes(status: "delivered")
   	render json: {status: :ok}
